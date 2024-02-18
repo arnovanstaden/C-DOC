4e5cc8d0f025660e146154a7a93c210ae7205387
arnovanstaden <arnovanstaden@gmail.com>
2024-02-18 17:43:18 +0100

    start pb integration

--- src/app/(website)/(shop)/shop/[id]/page.tsx
+++ src/app/(website)/(shop)/shop/[id]/page.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { updateCart } from '@utils/cart';
-import Link from 'next/link';
-import Image from 'next/image';
-import Section from '@components/website/layout/Section/Section';
-import styles from '../../styles/pages/shop/[id].module.scss';
+// import { useState } from 'react';
+// import { updateCart } from '@utils/cart';
+// import Link from 'next/link';
+// import Image from 'next/image';
+// import Section from '@components/website/layout/Section/Section';
+// import styles from '../../styles/pages/shop/[id].module.scss';
 import { generateCustomMetaData } from '@utils/metadata';
 
 export const generateMetadata = async () => {
@@ -19,72 +19,73 @@ export const generateMetadata = async () => {
 };
 
 const ShopProductPage = async () => {
+  return null;
   // State
-  const [quantity, setQuantity] = useState(1);
+  // const [quantity, setQuantity] = useState(1);
 
-  const product = undefined;
-  if (!product) return null;
+  // const product = undefined;
+  // if (!product) return null;
 
-  const ProductOption = () => {
-    if (product.price === 0 && product.digital) {
-      return (
-        <div className={styles.cart}>
-          <button className={styles.freebie}>
-            <a href={product.document} target="blank" download>Download Freebie</a>
-          </button>
-        </div>
-      );
-    }
-    return (
-      <div className={styles.cart}>
-        <div className={styles.quantity}>
-          <input min={1} type="number" name="quantity" value={quantity} onChange={e => setQuantity(parseInt(e.target.value))} />
-        </div>
-        <button className={styles.add} onClick={() => updateCart(product, quantity)}>
-          <i className="icon-local_grocery_store"></i>
-          Add To Cart
-        </button>
-      </div>
-    );
-  };
+  // const ProductOption = () => {
+  //   if (product.price === 0 && product.digital) {
+  //     return (
+  //       <div className={styles.cart}>
+  //         <button className={styles.freebie}>
+  //           <a href={product.document} target="blank" download>Download Freebie</a>
+  //         </button>
+  //       </div>
+  //     );
+  //   }
+  //   return (
+  //     <div className={styles.cart}>
+  //       <div className={styles.quantity}>
+  //         <input min={1} type="number" name="quantity" value={quantity} onChange={e => setQuantity(parseInt(e.target.value))} />
+  //       </div>
+  //       <button className={styles.add} onClick={() => updateCart(product, quantity)}>
+  //         <i className="icon-local_grocery_store"></i>
+  //         Add To Cart
+  //       </button>
+  //     </div>
+  //   );
+  // };
 
-  return (
-    <main>
-      <Section
-        noCross={true}
-        className={styles.product}
-      >
-        <div className={styles.shop}>
-          <Link href="/shop">
-            <button className="button button--border">
-              Back to Shop
-            </button>
-          </Link>
-        </div>
-        <div className={styles.grid}>
-          <div className={styles.image}>
-            <Image
-              src={product.thumbnail} alt="Article Image"
-              layout='fill'
-            />
-          </div>
-          <div className={styles.content}>
-            <p className={styles.category}>{product.category}</p>
-            <h2 className={styles.name}>{product.name}</h2>
-            <p className={styles.price}>{product.price > 0 ? `R ${product.price}` : 'Free'}</p>
-            <div className={styles.details}>
-              <p className={styles.active}>Description</p>
-            </div>
-            <p className={styles.description}>
-              {product.description}
-              {product.digital ? <span>This is a Digital Product</span> : null}
-            </p>
-            <ProductOption />
-          </div>
-        </div>
-      </Section>
-    </main>
-  );
+  // return (
+  //   <main>
+  //     <Section
+  //       noCross={true}
+  //       className={styles.product}
+  //     >
+  //       <div className={styles.shop}>
+  //         <Link href="/shop">
+  //           <button className="button button--border">
+  //             Back to Shop
+  //           </button>
+  //         </Link>
+  //       </div>
+  //       <div className={styles.grid}>
+  //         <div className={styles.image}>
+  //           <Image
+  //             src={product.thumbnail} alt="Article Image"
+  //             layout='fill'
+  //           />
+  //         </div>
+  //         <div className={styles.content}>
+  //           <p className={styles.category}>{product.category}</p>
+  //           <h2 className={styles.name}>{product.name}</h2>
+  //           <p className={styles.price}>{product.price > 0 ? `R ${product.price}` : 'Free'}</p>
+  //           <div className={styles.details}>
+  //             <p className={styles.active}>Description</p>
+  //           </div>
+  //           <p className={styles.description}>
+  //             {product.description}
+  //             {product.digital ? <span>This is a Digital Product</span> : null}
+  //           </p>
+  //           <ProductOption />
+  //         </div>
+  //       </div>
+  //     </Section>
+  //   </main>
+  // );
 };
 
 export default ShopProductPage;
--- src/app/(website)/(shop)/shop/page.tsx
+++ src/app/(website)/(shop)/shop/page.tsx
@@ -1,88 +1,87 @@
-import Link from 'next/link';
-import { useState, useRef } from 'react';
-import styles from '../../styles/pages/shop.module.scss';
-import { sortProducts } from '@utils/utils';
-import Section from '@components/website/layout/Section/Section';
-import Product from '@components/website/content/Product/Product';
-import { generateCustomMetaData } from '@utils/metadata';
+// import Link from 'next/link';
+// import { useState, useRef } from 'react';
+// import styles from '../../styles/pages/shop.module.scss';
+// import { sortProducts } from '@utils/utils';
+// import Section from '@components/website/layout/Section/Section';
+// import Product from '@components/website/content/Product/Product';
+// import { generateCustomMetaData } from '@utils/metadata';
 
-export const metadata = generateCustomMetaData({
-  title: 'Shop | C-DOC',
-  description: 'Browse C-DOC’s wide range of Medical Equipment, Clothing, Gear and E-Books.',
-});
+// export const metadata = generateCustomMetaData({
+//   title: 'Shop | C-DOC',
+//   description: 'Browse C-DOC’s wide range of Medical Equipment, Clothing, Gear and E-Books.',
+// });
 
 
-const ShopPage = async ({ products }) => {
+const ShopPage = async () => {
+  return null;
+
   // Refs
-  const sortRef = useRef();
+  // const sortRef = useRef();
 
-  // State
-  const [filter, setFilter] = useState('All Products');
-  const [productsToShow, setProductsToShow] = useState(sortProducts(products, 'name'));
+  // // State
+  // const [filter, setFilter] = useState('All Products');
+  // const [productsToShow, setProductsToShow] = useState(sortProducts(products, 'name'));
 
-  const product = undefined;
-  if (!product) return null;
+  // // Handlers
 
-  // Handlers
+  // const handleFilter = (clickedElement) => {
 
-  const handleFilter = (clickedElement) => {
+  //   // Active Category
+  //   const tabs = document.querySelectorAll(`.${styles.categories} button`);
+  //   tabs.forEach((elem) => {
+  //     elem.classList.remove(styles.active);
+  //   });
+  //   clickedElement.target.classList.add(styles.active);
+  //   setFilter(clickedElement.target.textContent);
+  // };
 
-    // Active Category
-    const tabs = document.querySelectorAll(`.${styles.categories} button`);
-    tabs.forEach((elem) => {
-      elem.classList.remove(styles.active);
-    });
-    clickedElement.target.classList.add(styles.active);
-    setFilter(clickedElement.target.textContent);
-  };
+  // const handleSort = () => {
+  //   const select = sortRef.current as HTMLSelectElement;
+  //   setProductsToShow(sortProducts([...products], select.value));
+  // };
 
-  const handleSort = () => {
-    const select = sortRef.current as HTMLSelectElement;
-    setProductsToShow(sortProducts([...products], select.value));
-  };
+  // return (
+  //   <main>
+  //     <Section
+  //       heading="Shop"
+  //       subHeading="Browse C-DOC’s wide range of Medical Equipment, Clothing, Gear and E-Books."
+  //       noCross={true}
+  //       className={styles.shop}
+  //     >
+  //       <div className={styles.categories}>
+  //         <button className={`${styles.button} ${styles.active}`} onClick={(e) => handleFilter(e)}>All Products</button>
+  //         <button className={styles.button} onClick={(e) => handleFilter(e)}>Medical Equipment</button>
+  //         <button className={styles.button} onClick={(e) => handleFilter(e)}>Clothing &amp; Gear</button>
+  //         <button className={styles.button} onClick={(e) => handleFilter(e)}>Guidance Documents</button>
+  //       </div>
+  //       <div className={styles.sort}>
+  //         <label htmlFor="sort">Sort By:</label>
+  //         <select name="sort" id="sort" onChange={handleSort} ref={sortRef}>
+  //           <option value="name">Name</option>
+  //           <option value="price">Price</option>
+  //         </select>
+  //       </div>
+  //       <div className={styles.grid}>
+  //         {productsToShow.map((product, index) => (
+  //           (filter === 'All Products' || filter === product.category) ? <Product {...product} key={index} /> : null
+  //         ))}
+  //       </div>
+  //     </Section>
 
-  return (
-    <main>
-      <Section
-        heading="Shop"
-        subHeading="Browse C-DOC’s wide range of Medical Equipment, Clothing, Gear and E-Books."
-        noCross={true}
-        className={styles.shop}
-      >
-        <div className={styles.categories}>
-          <button className={`${styles.button} ${styles.active}`} onClick={(e) => handleFilter(e)}>All Products</button>
-          <button className={styles.button} onClick={(e) => handleFilter(e)}>Medical Equipment</button>
-          <button className={styles.button} onClick={(e) => handleFilter(e)}>Clothing &amp; Gear</button>
-          <button className={styles.button} onClick={(e) => handleFilter(e)}>Guidance Documents</button>
-        </div>
-        <div className={styles.sort}>
-          <label htmlFor="sort">Sort By:</label>
-          <select name="sort" id="sort" onChange={handleSort} ref={sortRef}>
-            <option value="name">Name</option>
-            <option value="price">Price</option>
-          </select>
-        </div>
-        <div className={styles.grid}>
-          {productsToShow.map((product, index) => (
-            (filter === 'All Products' || filter === product.category) ? <Product {...product} key={index} /> : null
-          ))}
-        </div>
-      </Section>
-
-      <section className={styles.equipment}>
-        <div className="heading">
-          <h1>C-DOC Medical Kits.</h1>
-          <span />
-        </div>
-        <p>The C-DOC health and safety team design products with the specific knowledge and experience of commercial diving operations. These kits are not available for puchase online but can be ordered via our order form:</p>
-        <button className="button">
-          <Link href="/services#equipment">
-            Medical Kits Enquiry
-          </Link>
-        </button>
-      </section>
-    </main>
-  );
+  //     <section className={styles.equipment}>
+  //       <div className="heading">
+  //         <h1>C-DOC Medical Kits.</h1>
+  //         <span />
+  //       </div>
+  //       <p>The C-DOC health and safety team design products with the specific knowledge and experience of commercial diving operations. These kits are not available for puchase online but can be ordered via our order form:</p>
+  //       <button className="button">
+  //         <Link href="/services#equipment">
+  //           Medical Kits Enquiry
+  //         </Link>
+  //       </button>
+  //     </section>
+  //   </main>
+  // );
 };
 
 export default ShopPage;
