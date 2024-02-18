4411cfbec88d6b4cc72dfd3e67b97ca05355e5d3
arnovanstaden <arnovanstaden@gmail.com>
2024-02-18 14:02:26 +0100

    update dependencies

--- custom-next-sitemap.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    siteUrl: 'https://www.c-doc.co.za',
-    generateRobotsTxt: true, // (optional),
-    exclude: ['/404', '/cart', '/cart*'],
-    changefreq: 'daily',
-    priority: 0.8,
-    sitemapSize: 7000,
-}
