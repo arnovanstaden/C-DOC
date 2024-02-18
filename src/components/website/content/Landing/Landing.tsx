1ccfb7e70002cb5373d105f76f8e8716d659c3bc
arnovanstaden <arnovanstaden@gmail.com>
2024-02-18 15:35:57 +0100

    update breakpoints

--- src/components/website/content/Landing/Landing.tsx
+++ src/components/website/content/Landing/Landing.tsx
@@ -1,9 +1,4 @@
 import Image from 'next/image';
-
-// Components
-import Header from '../../layout/Header/Header';
-
-// Styles
 import styles from './landing.module.scss';
 
 interface TLanding {
@@ -16,7 +11,6 @@ export default function Landing(props: TLanding) {
   return (
     <section className={styles.landing}>
       <div className={styles.content}>
-        <Header />
         {props.custom ? props.children :
           <div className={styles.text}>
             {props.children}
